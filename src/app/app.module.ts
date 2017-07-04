eb944ce152e804fc2dac0cf10516a885b6767fad
Adrián Brito <adrbrpa1988@gmail.com>
2017-07-04 14:20:45 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,6 +15,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  animations: [
+    trigger('colorChange', [
+      state('black', style({ background: 'black' })),
+      transition('* => black', animate('3000ms')),
+      state('red', style({ background: 'red' })),
+      transition('* => red', animate('3000ms')),
+      state('green', style({ background: 'green' })),
+      transition('* => green', animate('3000ms')),
+      state('blue', style({ background: 'blue' })),
+      transition('* => blue', animate('3000ms')),
+      state('yellow', style({ background: 'yellow' })),
+      transition('* => yellow', animate('3000ms')),
+      state('orange', style({ background: 'orange' })),
+      transition('* => orange', animate('3000ms')),
+      state('aqua', style({ background: 'aqua' })),
+      transition('* => aqua', animate('3000ms')),
+      state('white', style({ background: 'white' })),
+      transition('* => white', animate('3000ms')),
+      state('pink', style({ background: 'pink' })),
+      transition('* => pink', animate('3000ms'))
+    ])
+  ],
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    private colorCount: number = 0;
+    private currentColor: string;
+    colorArr: string[] = ['red', 'green', 'blue', 'yellow', 'orange', 'aqua', 'pink', 'white'];
 
-  }
+    constructor(public navCtrl: NavController) {
+
+    }
+
+    changeColor() {
+        this.colorCount++;
+        if (this.colorCount < this.colorArr.length) {
+            this.currentColor = this.colorArr[this.colorCount];
+        } else {
+            this.colorCount = 0;
+            this.currentColor = this.colorArr[0];
+        }
+    }
 
 }
