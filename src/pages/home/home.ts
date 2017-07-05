5d27fcc76f223274151fec28473edef0561f5e00
Adrián Brito <adrbrpa1988@gmail.com>
2017-07-05 12:25:50 +0200

    Updated

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger, keyframes } from '@angular/animations';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -26,6 +26,7 @@ import { NavController } from 'ionic-angular';
       state('pink', style({ background: 'pink' })),
       transition('* => pink', animate('3000ms'))
     ]),
+      /*
       trigger('sizeChange', [
           state('small', style({
               transform: 'scale(1)',
@@ -33,10 +34,49 @@ import { NavController } from 'ionic-angular';
           state('large', style({
               transform: 'scale(1.2)',
           })),
-          transition('small <=> large', animate('300ms ease-in', style({
-              transform: 'translateY(40px)'
-          }))),
+          transition('small <=> large', animate('300ms ease-in', keyframes([
+              style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+              style({opacity: 1, transform: 'translateY(35px)',  offset: 0.5}),
+              style({opacity: 1, transform: 'translateY(0)',     offset: 1.0})
+          ]))),
       ]),
+      */
+      trigger('resize', [
+          state('small', style({
+              transform: 'scale(1)',
+          })),
+          state('large', style({
+              transform: 'scale(1.3)',
+          })),
+          transition('small <=> large', animate('300ms ease-in-out')),
+      ]),
+      trigger('shake', [
+          state('start', style ({
+              perspective: '1000px'
+          })),
+          state('end', style ({
+              perspective: '1000px'
+          })),
+          transition('start <=> end', animate('5s cubic-bezier(.36,.07,.19,.97)', keyframes([
+              style({transform: 'translate3d(-1px, 0, 0)', offset: 0.1}),
+              style({transform: 'translate3d(2px, 0, 0)', offset: 0.2}),
+              style({transform: 'translate3d(-4px, 0, 0)', offset: 0.3}),
+              style({transform: 'translate3d(4px, 0, 0)', offset: 0.4}),
+              style({transform: 'translate3d(-4px, 0, 0)', offset: 0.5}),
+              style({transform: 'translate3d(4px, 0, 0)', offset: 0.6}),
+              style({transform: 'translate3d(-4px, 0, 0)', offset: 0.7}),
+              style({transform: 'translate3d(2px, 0, 0)', offset: 0.8}),
+              style({transform: 'translate3d(-1px, 0, 0)', offset: 0.9}),
+          ])))
+      ]),
+      /*
+      trigger('scale', [
+          transition('* <=> *', animate('1s ease-in-out', keyframes([
+              style({ transform: 'scale(0.5)' }),
+              style({ transform: 'scale(1)' })
+          ])))
+      ])
+      */
   ],
 })
 export class HomePage {
@@ -45,7 +85,13 @@ export class HomePage {
     private currentColor: string;
     colorArr: string[] = ['red', 'green', 'blue', 'yellow', 'orange', 'aqua', 'pink', 'white'];
 
-    private currentSize: string = 'small';
+    //private currentSize: string = 'small';
+
+    private shake: string = 'start';
+
+    private resize: string = 'small';
+
+    //private scale: string = 'scale';
 
     constructor(public navCtrl: NavController) {
 
@@ -62,8 +108,19 @@ export class HomePage {
         }
     }
 
+    /*
     changeSize() {
         this.currentSize = (this.currentSize === 'small' ? 'large' : 'small');
+    }
+    */
+
+    resizeButton() {
+        this.resize = (this.resize === 'small' ? 'large' : 'small');
+    }
+
+    shakeIcon() {
+        console.log("shakeIcon");
+        this.shake = (this.shake === 'start' ? 'end' : 'start');
     }
 
 }
